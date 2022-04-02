2b538a052ef0096fd245bc1c44bd7b4bb376a6c0
denny-saryanto <dennysaryanto7@gmail.com>
2022-04-02 09:52:48 +0700

    update

--- main.py
+++ main.py
@@ -285,5 +285,5 @@ def api_model_train():
         return jsonify({'status' : 'error request'})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=5556)
+    app.run(debug=True, port=33507)
 #-- Web Based End --#
